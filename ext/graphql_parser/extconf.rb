de94ac8344752016700b1d5d1c1fbd156d09d5c9
Evan Huus <eapache@gmail.com>
2015-09-14 14:19:14 -0400

    Fix makefile, proof-of-concept call

--- ext/graphql_parser/extconf.rb
+++ ext/graphql_parser/extconf.rb
@@ -4,4 +4,4 @@ dir_config('graphql', '/usr/local/include', '/usr/local/lib')
 
 abort 'missing libgraphqlparser' unless have_library 'graphqlparser'
 
-create_makefile 'graphql-parser/graphql-parser'
+create_makefile 'graphql_parser/graphql_parser'
--- ext/graphql_parser/graphql_parser.c
+++ ext/graphql_parser/graphql_parser.c
@@ -1,12 +1,26 @@
 #include <ruby.h>
 
-#include "GraphQLAstNode.h"
+#include "GraphQLParser.h"
+#include "GraphQLASTNode.h"
+
+VALUE parse() {
+    struct GraphQLAstNode *n;
+
+    n = graphql_parse_string("", NULL);
+
+    if (n == NULL) {
+        return Qnil;
+    }
+    graphql_node_free(n);
+
+    return rb_cObject;
+}
 
 void
 Init_graphql_parser(void) {
     VALUE m, c;
 
     m = rb_define_module("GraphQL");
-    c = rb_define_class_under(m, "Node", rb_cObject);
-    c = rb_define_class_under(m, "Location", rb_cObject);
+    c = rb_define_class_under(m, "Parser", rb_cObject);
+    rb_define_method(c, "parse", parse, 0);
 }
