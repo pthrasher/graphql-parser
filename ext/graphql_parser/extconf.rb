4294e3f714443630beecf3fdb9a39ab2340fdea3
Evan Huus <eapache@gmail.com>
2015-09-16 20:26:21 +0000

    WIP self-build

--- ext/graphql_parser/extconf.rb
+++ ext/graphql_parser/extconf.rb
@@ -1,6 +1,14 @@
 require 'mkmf'
 
-dir_config('graphql', '/usr/local/include/graphqlparser', '/usr/local/lib')
+ROOT = '../../../..'
+
+`cd #{ROOT}/libgraphqlparser && cmake . && make && cd -`
+
+dir_config('graphql', "#{ROOT}/libgraphqlparser", "#{ROOT}/libgraphqlparser")
+
+AST = "#{ROOT}/libgraphqlparser/ast"
+`PYTHONPATH=$PYTHONPATH:#{ROOT}/generators python #{AST}/ast.py ruby_header_gen #{AST}/ast.ast > #{ROOT}/ext/graphql_parser/graphql_ruby.h`
+`PYTHONPATH=$PYTHONPATH:#{ROOT}/generators python #{AST}/ast.py ruby_impl_gen #{AST}/ast.ast > #{ROOT}/ext/graphql_parser/graphql_ruby.c`
 
 abort 'missing libgraphqlparser' unless have_library 'graphqlparser'
 
